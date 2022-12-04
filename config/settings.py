9628d275fae8fc5352265dbccb569c15310caa74
zharkynaskat <61348606+zharkynaskat@users.noreply.github.com>
2022-12-05 00:57:04 +0600

    Create static files. Work on admin and web appearance

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
+    "mptt",
+    "treewidget",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my apps
-    "mptt",
     "django_mptt_admin",
     "tree",
 ]
@@ -56,11 +56,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "config.urls"
 
+TEMPLATE_DIR = [os.path.join(BASE_DIR, 'templates/')]
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": TEMPLATE_DIR,
+
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,12 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- tree/views.py
+++ tree/views.py
@@ -1,25 +1,35 @@
-from django.views.generic import ListView
+from django.shortcuts import render
 
 from tree.models import Employee, Department
 
 
-class DepartmentListView(ListView):
-    model = Department
-    template_name = "department_list.html"
+# class DepartmentListView(ListView):
+#     model = Department
+#     template_name = "department_list.html"
+#
+#
+# class EmployeeByDepartmentView(ListView):
+#     model = Employee
+#     context_object_name = 'employees'
+#     template_name = 'employee_list.html'
+#
+#     def get_queryset(self):
+#         self.department = Department.objects.get(name=self.kwargs['name'])
+#         queryset = Employee.objects.filter(department=self.department)
+#         print(queryset)
+#         return queryset
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['name'] = self.department
+#         return context
 
 
-class EmployeeByDepartmentView(ListView):
-    model = Employee
-    context_object_name = 'employees'
-    template_name = 'employee_list.html'
+def index(request):
+    dep_queryset = Department.objects.all()
+    empl_queryset = Employee.objects.all()
 
-    def get_queryset(self):
-        self.department = Department.objects.get(name=self.kwargs['name'])
-        queryset = Employee.objects.filter(department=self.department)
-        print(queryset)
-        return queryset
+    return render(request, "index.html", context={'departments': dep_queryset,
+                                                  'employees': empl_queryset})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['name'] = self.department
-        return context
+
